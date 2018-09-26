2a3e0b74abf189a08957703583317632c946032a
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-09-25 18:05:30 -0700

    fix bugs:

--- password-keeper.py
+++ password-keeper.py
@@ -1,21 +1,23 @@
-!# python3
+#! python3
 # password keeper
 # python script to hold app passwords
+
+import sys
+import pyperclip
 
 PASSWORDS = {'email': '/6ufAbx[#W>]N~',
             'blog': 'O-n\1,?"|~k$Rl',
             'safe': 'Nca6osx5!G&[Dx',
             'locker': 'l!"hH(@"3C!1Ez'}
 
-import sys.pyperclip
 if len(sys.argv) < 2:
-  print('Usage: python password-keeper.py [account] - copy account password'
+  print('Usage: python password-keeper.py [account] - copy account password')
   sys.exit()
 
 account = sys.argv[1]     # first command line arg is the account name
 
-if account in PASSWORDS
+if account in PASSWORDS:
   pyperclip.copy(PASSWORDS[account])
   print('Password for ' + account + ' copied to clipboard')
-else
+else:
   print('There is no account named ' + account)
