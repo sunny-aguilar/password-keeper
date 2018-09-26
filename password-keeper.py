e4f92096fb2734ce30f2ebcb2bc713e303f6087f
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-09-25 17:03:18 -0700

    add if logic

--- password-keeper.py
+++ password-keeper.py
@@ -6,6 +6,12 @@ PASSWORDS = {'email': '/6ufAbx[#W>]N~',
             'blog': 'O-n\1,?"|~k$Rl',
             'safe': 'Nca6osx5!G&[Dx',
             'locker': 'l!"hH(@"3C!1Ez'}
-            
+
+import sys
+if len(sys.argv) < 2:
+  print('Usage: python password-keeper.py [account] - copy account password'
+  sys.exit()
+
+account = sys.argv[1]     # first command line arg is the account name
 
 
