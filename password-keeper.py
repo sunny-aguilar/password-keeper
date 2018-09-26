413e9883ecc738ff0907f92ee23acbc0c83ef06a
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-09-25 17:01:17 -0700

    add key value pairs

--- password-keeper.py
+++ password-keeper.py
@@ -1,9 +1,11 @@
 !# python3
-
-
 # password keeper
 # python script to hold app passwords
 
-
+PASSWORDS = {'email': '/6ufAbx[#W>]N~',
+            'blog': 'O-n\1,?"|~k$Rl',
+            'safe': 'Nca6osx5!G&[Dx',
+            'locker': 'l!"hH(@"3C!1Ez'}
+            
 
 
