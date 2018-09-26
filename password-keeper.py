3b5c614e9cb0b7390ecfc4a1b3c90f52160ced26
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-09-25 17:07:48 -0700

    complete file

--- password-keeper.py
+++ password-keeper.py
@@ -7,11 +7,15 @@ PASSWORDS = {'email': '/6ufAbx[#W>]N~',
             'safe': 'Nca6osx5!G&[Dx',
             'locker': 'l!"hH(@"3C!1Ez'}
 
-import sys
+import sys.pyperclip
 if len(sys.argv) < 2:
   print('Usage: python password-keeper.py [account] - copy account password'
   sys.exit()
 
 account = sys.argv[1]     # first command line arg is the account name
 
-
+if account in PASSWORDS
+  pyperclip.copy(PASSWORDS[account])
+  print('Password for ' + account + ' copied to clipboard')
+else
+  print('There is no account named ' + account)
